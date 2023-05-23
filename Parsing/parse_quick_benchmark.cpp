ff0d6c34512fc7da5a22ec2933f81407c1a571a0
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-05-23 20:25:30 +0300

    Add parse qucik benchmark

---
+++ Parsing/parse_quick_benchmark.cpp
@@ -0,0 +1,37 @@
+// Run at https://quick-bench.com/
+
+#include <charconv>
+#include <sstream>
+
+static void ParseStof(benchmark::State& state) {
+  constexpr auto numberString = "1.5";
+
+  for (auto _ : state) {
+    float value = std::stof(numberString);
+    benchmark::DoNotOptimize(value);
+  }
+}
+BENCHMARK(ParseStof);
+
+static void ParseStream(benchmark::State& state) {
+  constexpr auto numberString = "1.5";
+
+  for (auto _ : state) {
+    std::istringstream os{numberString};
+    float value{};
+    os >> value;
+    benchmark::DoNotOptimize(value);
+  }
+}
+BENCHMARK(ParseStream);
+
+static void ParseFromChars(benchmark::State& state) {
+  constexpr std::string_view numberString = "1.5";
+
+  for (auto _ : state) {
+    float value{};
+    std::from_chars(std::begin(numberString), std::end(numberString), value);
+    benchmark::DoNotOptimize(value);
+  }
+}
+BENCHMARK(ParseFromChars);
