c08d2a4de39f7416d11853c2c93e9c3d32aebbde
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-05-24 21:05:45 +0300

    Extend float representation sample

--- Rounding/float_representation.cpp
+++ Rounding/float_representation.cpp
@@ -12,6 +12,19 @@ int main() {
   constexpr std::string_view literal =
       R"lit(3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117068)lit";
 
+
+  std::cout << "Default precision:\n";
+
+  // 3.14159
+  std::cout << "Float:       " << pi_f << '\n';
+
+  // 3.14159
+  std::cout << "Double:      " << pi_d << '\n';
+
+  // 3.14159
+  std::cout << "Long Double: " << pi_ld << '\n';
+
+  std::cout << "Maximal precision:\n";
   std::cout.precision(literal.size());
 
   std::cout << "Literal:     " << literal << '\n';
