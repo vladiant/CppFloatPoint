02f6a025afb7f93d97d43bc42ee5f05e5bdc4212
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-06-12 07:17:37 +0300

    Updta float point comparison

--- Comparison/float_special_values_comparison.cpp
+++ Comparison/float_special_values_comparison.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <limits>
 
 auto floatToInt(float value) {
   static_assert(sizeof(value) == sizeof(uint32_t),
@@ -18,8 +19,24 @@ int main() {
   std::cout << " 0.0f : "
             << " " << floatToInt(0.0f) << '\n';
 
-  std::cout << "NAN != NAN : " << std::boolalpha << (NAN != NAN) << '\n';
-  std::cout << " NAN : " << std::hex << floatToInt(NAN) << '\n';
+  float valueCNaN = NAN;
+  std::cout << " NAN != NAN                 : " << std::boolalpha
+            << (valueCNaN != valueCNaN) << '\n';
+  std::cout << " NAN                        : " << std::hex
+            << floatToInt(valueCNaN) << '\n';
+
+  float valueQuietNaN = std::numeric_limits<float>::quiet_NaN();
+  std::cout << " quiet_NaN                  : " << std::hex
+            << floatToInt(valueQuietNaN) << '\n';
+
+  float valueSignalNaN = std::numeric_limits<float>::signaling_NaN();
+  std::cout << " signaling_NaN              : " << std::hex
+            << floatToInt(valueSignalNaN) << '\n';
+
+  std::cout << " signaling_NaN != quiet_NaN : " << std::boolalpha
+            << (valueSignalNaN != valueQuietNaN) << '\n';
+  std::cout << " quiet_NaN != quiet_NaN     : " << std::boolalpha
+            << (valueQuietNaN != valueQuietNaN) << '\n';
 
   return 0;
 }
